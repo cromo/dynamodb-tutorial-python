4f9834f5912896ff939208b0ed6aea1ca4069d3e
Cristian Romo <cristian.g.romo@gmail.com>
2019-11-22 23:08:31 -0600

    Add step 1: create a table

--- dynamodb_tutorial_python/__init__.py
+++ dynamodb_tutorial_python/__init__.py
@@ -1,6 +1,40 @@
-__version__ = '0.1.0'
+import boto3
 
 def main():
-    print("hey")
+    step_1_create_a_table()
 
+def step_1_create_a_table():
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
+
+    table = dynamodb.create_table(
+        TableName='Movies',
+        KeySchema=[
+            {
+                'AttributeName': 'year',
+                'KeyType': 'HASH'  # Partition key
+            },
+            {
+                'AttributeName': 'title',
+                'KeyType': 'RANGE'  # Sort key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'year',
+                'AttributeType': 'N'
+            },
+            {
+                'AttributeName': 'title',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    )
+
+    print("Table status:", table.table_status)
+
+__version__ = '0.1.0'
 __main__ = main

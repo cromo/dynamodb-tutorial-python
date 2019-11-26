68704a0ce56596fb7fd166a1345ad4c1254cf7dd
Cristian Romo <cristian.g.romo@gmail.com>
2019-11-26 01:30:22 -0600

    Add step 3.2: read an item

--- dynamodb_tutorial_python/__init__.py
+++ dynamodb_tutorial_python/__init__.py
@@ -1,10 +1,14 @@
 import boto3
 import json
 import decimal
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
 
 def main():
     # step_1_create_a_table()
-    step_2_load_sample_data()
+    # step_2_load_sample_data()
+    # step_3_1_create_a_new_item()
+    step_3_2_read_an_item()
 
 def step_1_create_a_table():
     dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
@@ -60,6 +64,59 @@ def step_2_load_sample_data():
                     'info': info
                 }
             )
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if abs(o) % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+def step_3_1_create_a_new_item():
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
+
+    table = dynamodb.Table('Movies')
+
+    title = "The Big New Movie"
+    year = 2015
+
+    response = table.put_item(
+        Item={
+            'year': year,
+            'title': title,
+            'info': {
+                'plot': "Nothing happens at all.",
+                'rating': decimal.Decimal(0)
+            }
+        }
+    )
+
+    print("PutItem succeeded:")
+    print(json.dumps(response, indent=4, cls=DecimalEncoder))
+
+def step_3_2_read_an_item():
+    dynamodb = boto3.resource("dynamodb", region_name='us-west-2', endpoint_url="http://localhost:8000")
+
+    table = dynamodb.Table('Movies')
+
+    title = "The Big New Movie"
+    year = 2015
+
+    try:
+        response = table.get_item(
+            Key={
+                'year': year,
+                'title': title
+            }
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        item = response['Item']
+        print("GetItem succeeded:")
+        print(json.dumps(item, indent=4, cls=DecimalEncoder))
 
 __version__ = '0.1.0'
 __main__ = main

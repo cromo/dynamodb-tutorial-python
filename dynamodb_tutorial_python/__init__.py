620b115bad04bb809330c6f8a328a558fe6123e3
Cristian Romo <cristian.g.romo@gmail.com>
2019-11-26 01:10:58 -0600

    Add step 2: load sample data

--- dynamodb_tutorial_python/__init__.py
+++ dynamodb_tutorial_python/__init__.py
@@ -1,7 +1,10 @@
 import boto3
+import json
+import decimal
 
 def main():
-    step_1_create_a_table()
+    # step_1_create_a_table()
+    step_2_load_sample_data()
 
 def step_1_create_a_table():
     dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
@@ -35,6 +38,28 @@ def step_1_create_a_table():
     )
 
     print("Table status:", table.table_status)
+
+def step_2_load_sample_data():
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
+
+    table = dynamodb.Table('Movies')
+
+    with open("moviedata.json") as json_file:
+        movies = json.load(json_file, parse_float=decimal.Decimal)
+        for movie in movies:
+            year = int(movie['year'])
+            title = movie['title']
+            info = movie['info']
+
+            print("Adding movie:", year, title)
+
+            table.put_item(
+                Item={
+                    'year': year,
+                    'title': title,
+                    'info': info
+                }
+            )
 
 __version__ = '0.1.0'
 __main__ = main
